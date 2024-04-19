3d9fbeb3ec42ed9062f7f83b2fb360faeb0407fa
chinh-nc <congchinh705@gmail.com>
2024-04-19 22:10:19 +0700

    feat: :sparkles: add calculate max profit level 2
    add function to calculate max profit for level 2 and add test to this case

--- best_time_by_sell/best_time_by_sell.go
+++ best_time_by_sell/best_time_by_sell.go
@@ -12,3 +12,13 @@ func MaxProfit(prices []int) int {
 	}
 	return profit
 }
+
+func MaxProfitLevel2(prices []int) int {
+	profit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			profit += prices[i] - prices[i-1]
+		}
+	}
+	return profit
+}
--- best_time_by_sell/best_time_by_sell_test.go
+++ best_time_by_sell/best_time_by_sell_test.go
@@ -19,3 +19,29 @@ func TestMaxProfitReturnZero(t *testing.T) {
 		t.Fatalf("MaxProfit() = %v, want %v", result, want)
 	}
 }
+
+func TestMaxProfitLevel2Correct(t *testing.T) {
+	prices := []int{7,1,5,3,6,4}
+	want := 7
+	result := MaxProfitLevel2(prices)
+	if result != want {
+		t.Fatalf("MaxProfit() = %v, want %v", result, want)
+	}
+}
+
+func TestMaxProfitLevel2Increment(t *testing.T) {
+	prices := []int{1,2,3,4,5}
+	want := 4
+	result := MaxProfitLevel2(prices)
+	if result != want {
+		t.Fatalf("MaxProfit() = %v, want %v", result, want)
+	}
+}
+func TestMaxProfitLevel2NoProfit(t *testing.T) {
+	prices := []int{7,6,4,3,1}
+	want := 0
+	result := MaxProfitLevel2(prices)
+	if result != want {
+		t.Fatalf("MaxProfit() = %v, want %v", result, want)
+	}
+}
